6cf89f1eb94678a0b0f9c7ffa45df575c282d8d7
Sanjeet Agrawal <55800083+Sanjeet09@users.noreply.github.com>
2021-12-06 21:30:04 +0530

    Create LC217.py

---
+++ Arrays/LC217.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        num = set(nums)
+        if len(nums) != len(num):
+            return True
+        else:
+            return False
